9482c53d6dde563c89db7d628ee80f4f29bbb17f
deverasrenato <renatoveras@msn.com>
2022-12-16 08:40:46 -0300

    BranchMain
    Raiz do branch

---
+++ branch.py
@@ -0,0 +1,10 @@
+lista = []
+
+for recebe in range(2):
+    nome = input('Nome: ')
+    idade = input('Idade: ')
+    profissao = input('Profissao: ')
+    lista.append(nome)
+    lista.append(idade)
+    lista.append(profissao)
+print(lista)
